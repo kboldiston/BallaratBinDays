d4dd9424ca7151e3dff42198e51fa371fba4935c
kboldiston <k.boldiston@gmail.com>
2021-05-25 00:00:07 +1000

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Thing is true', () {
-    expect(1, 1);
+    // comment to get the build to run again
+    expect(1, 1); 
   });
 }
